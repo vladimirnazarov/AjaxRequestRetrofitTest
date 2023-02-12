3c85902b06a230ddf50c44b9f90d00a401275451
Vladimir Nazarov <tooldiamond2014@gmail.com>
2023-02-12 21:57:10 +0300

    Response read was made

--- app/src/main/java/com/vnazarov/testtaskpostrequest/MainActivity.kt
+++ app/src/main/java/com/vnazarov/testtaskpostrequest/MainActivity.kt
@@ -8,8 +8,7 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import org.json.JSONObject
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
@@ -25,12 +24,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var editText: EditText
     private lateinit var textView: TextView
 
-    private val builder = Retrofit.Builder()
-        .baseUrl("https://corpus.by/")
-        .addConverterFactory(GsonConverterFactory.create())
-
-    private val retrofit = builder.build()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -42,41 +35,7 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Edit text is empty", Toast.LENGTH_SHORT).show()
             } else {
 
-                val client = OkHttpClient.Builder().build()
-                val mediaType = "application/x-www-form-urlencoded".toMediaTypeOrNull()
-                val body = RequestBody.create(
-                    mediaType,
-                    "text=Груша цвіла апошні год.&checkbox1=0&checkbox2=1&checkbox3=1&mode=radiobutton2&selector=option1"
-                )
-                val request = Request.Builder()
-                    .url("https://corpus.by/ServiceDemonstrator/api.php")
-                    .method("POST", body)
-                    .addHeader("Content-Type", "application/x-www-form-urlencoded")
-                    .build()
-
-                client.newCall(request).enqueue(object : Callback {
-                    override fun onFailure(call: Call, e: IOException) {
-                        e.printStackTrace()
-                    }
-
-                    override fun onResponse(call: Call, response: Response) {
-                        response.use {
-                            if (!response.isSuccessful) throw IOException("Unexpected code $response")
-
-                            var str = response.body!!.string()
-                            str = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                                Html.fromHtml(str, Html.FROM_HTML_MODE_LEGACY).toString()
-                            } else {
-                                Html.fromHtml(str).toString()
-                            }
-                            runOnUiThread { textView.text = str }
-
-                            println(str)
-//                            println(response.body!!.string())
-                        }
-                    }
-                })
-
+                readResponse()
             }
         }
     }
@@ -103,7 +62,7 @@ class MainActivity : AppCompatActivity() {
 
         val itemSelectedSpinner = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                option = p2
+                option = p2 + 1
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {}
@@ -123,18 +82,49 @@ class MainActivity : AppCompatActivity() {
         }
 
         check1.setOnClickListener {
-            dCheck1 = if (it.isActivated) 1
+            dCheck1 = if (check1.isChecked) 1
             else 0
         }
 
         check2.setOnClickListener {
-            dCheck2 = if (it.isActivated) 1
+            dCheck2 = if (check2.isChecked) 1
             else 0
         }
 
         check3.setOnClickListener {
-            dCheck3 = if (it.isActivated) 1
+            dCheck3 = if (check3.isChecked) 1
             else 0
         }
+    }
+
+    private fun readResponse(){
+        val client = OkHttpClient.Builder().build()
+        val mediaType = "application/x-www-form-urlencoded".toMediaTypeOrNull()
+        val body = RequestBody.create(
+            mediaType,
+            "text=${editText.text}.&checkbox1=$dCheck1&checkbox2=$dCheck2&checkbox3=$dCheck3&mode=radiobutton$radio&selector=option$option"
+        )
+        val request = Request.Builder()
+            .url("https://corpus.by/ServiceDemonstrator/api.php")
+            .method("POST", body)
+            .addHeader("Content-Type", "application/x-www-form-urlencoded")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                    val jObject = response.body!!.string()
+                    val str = JSONObject(jObject).getString("result")
+
+                    runOnUiThread { textView.text = str }
+                }
+            }
+        })
     }
 }
