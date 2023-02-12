0cb3bf6e7d36d099ba5be5c717e0fff0495f16e1
Vladimir Nazarov <tooldiamond2014@gmail.com>
2023-02-12 19:02:41 +0300

    Request update

--- app/src/main/java/com/vnazarov/testtaskpostrequest/MainActivity.kt
+++ app/src/main/java/com/vnazarov/testtaskpostrequest/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.vnazarov.testtaskpostrequest
 
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import okhttp3.ResponseBody
-import retrofit2.*
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,16 +38,50 @@ class MainActivity : AppCompatActivity() {
         initItems()
 
         button.setOnClickListener {
-            if (editText.text.isEmpty()){
+            if (editText.text.isEmpty()) {
                 Toast.makeText(this, "Edit text is empty", Toast.LENGTH_SHORT).show()
             } else {
 
-                executeSendFeedbackForm("test", 0, 0, 0, "radiobutton1", "option2")
+                val client = OkHttpClient.Builder().build()
+                val mediaType = "application/x-www-form-urlencoded".toMediaTypeOrNull()
+                val body = RequestBody.create(
+                    mediaType,
+                    "text=Груша цвіла апошні год.&checkbox1=0&checkbox2=1&checkbox3=1&mode=radiobutton2&selector=option1"
+                )
+                val request = Request.Builder()
+                    .url("https://corpus.by/ServiceDemonstrator/api.php")
+                    .method("POST", body)
+                    .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                    .build()
+
+                client.newCall(request).enqueue(object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                    override fun onResponse(call: Call, response: Response) {
+                        response.use {
+                            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                            var str = response.body!!.string()
+                            str = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                                Html.fromHtml(str, Html.FROM_HTML_MODE_LEGACY).toString()
+                            } else {
+                                Html.fromHtml(str).toString()
+                            }
+                            runOnUiThread { textView.text = str }
+
+                            println(str)
+//                            println(response.body!!.string())
+                        }
+                    }
+                })
+
             }
         }
     }
 
-    private fun initItems(){
+    private fun initItems() {
         spinner = findViewById(R.id.spinner)
         check1 = findViewById(R.id.check1)
         check2 = findViewById(R.id.check2)
@@ -56,10 +94,14 @@ class MainActivity : AppCompatActivity() {
         textView = findViewById(R.id.textView)
 
         val optionsList = arrayListOf("Опцыя #1", "Опцыя #2", "Опцыя #3")
-        val adapter = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, optionsList)
+        val adapter = ArrayAdapter(
+            this,
+            androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+            optionsList
+        )
         spinner.adapter = adapter
 
-        val itemSelectedSpinner = object: AdapterView.OnItemSelectedListener {
+        val itemSelectedSpinner = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 option = p2
             }
@@ -94,22 +136,5 @@ class MainActivity : AppCompatActivity() {
             dCheck3 = if (it.isActivated) 1
             else 0
         }
-    }
-
-    private fun executeSendFeedbackForm(text: String, check1: Int, check2: Int, check3: Int, mode: String, option: String){
-        val apiService = retrofit.create(APIService::class.java)
-
-        val call = apiService.sendUserFeedback(text, check1, check2, check3, mode, option)
-
-        call.enqueue(object: Callback<ResponseBody>{
-            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                Toast.makeText(this@MainActivity, "Success!", Toast.LENGTH_SHORT).show()
-                println(response)
-            }
-
-            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                Toast.makeText(this@MainActivity, "Failure", Toast.LENGTH_SHORT).show()
-            }
-        })
     }
 }
